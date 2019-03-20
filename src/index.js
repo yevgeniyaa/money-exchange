04b3366295ec396a9b6c816f21d9890b4cdcabf0
Yevgeniya.Anossova <Yevgeniya.Anossova@wolterskluwer.com>
2019-03-20 12:18:59 +0600

    implement task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    const coins = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+
+    let result = {};
+
+    Object.keys(coins).forEach(coin => {
+        const value = coins[coin];
+        const amount = Math.floor(currency / value);
+        if (amount > 0) {
+            result[coin] = amount;
+            currency -= value * amount;
+        }
+    });
+
+    return result;
+};
